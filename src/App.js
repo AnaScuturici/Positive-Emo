a9c4146f441349717ec87bcc529d0ffde817b74c
Motoki-tech <>
2022-01-05 18:57:37 +0900

    update

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import "./App.css";
 import React from "react";
 import axios from "axios";
 import bocco from "./img_bocco.png";
+import e from "express";
 
 function App() {
-  const click = async () => {
+  const click = async (e) => {
+    e.preventDefault();
     const text = await axios.get("/api");
     //console.log(text.data);
 
     await axios.post(
       "https://platform-api.bocco.me/v1/rooms/272f4f0f-b20c-4b18-9f4e-0cba6e9fae17/messages/text",
       {
+        text: text.data,
+      },
+      {
         headers: {
-          Authorization: "Bearer 6bd383de-ae67-4909-88ce-ec6775c565fe",
+          Authorization: `Bearer ${await refresh()}`,
           "Content-Type": "application/json",
-        },
-        body: {
-          text: text.data,
         },
       }
     );
