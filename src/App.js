69424ec82b69110f67d4aa4aef99f1e2db8ba997
mei-omomo <l.3.llxl@gmail.com>
2022-01-05 10:51:21 +0900

    feat/大枠

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  const click = async () => {
+    await fetch("https://platform-api.bocco.me/v1/rooms/272f4f0f-b20c-4b18-9f4e-0cba6e9fae17/messages/text", {
+      method: 'post',
+      headers: {
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyZTQ4OTM2Ni0zODZhLTQ0NDgtOWYzMy05NWU3MzIyMzU1MzgiLCJpc3MiOiJwbGF0Zm9ybS1hcGkiLCJleHAiOjE2NDEzNDk5NjEsInBsYW4iOiJmcmVlIiwiaWF0IjoxNjQxMzQ2MzYxfQ.a1lih5iWb08I5g9H7NubynzbIBUjZJFmaZnu8zphwztyXMWrq0Euq2fxdTEr_KDWQC7sDRRGeCm6bFsHPrhUMg',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        text:"Hello!"
+      }),
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='title'>Positive Emo</h1>
+      <div className='button-area'>
+        <button onClick={()=> click()}>emoちゃんのimg？</button>
+      </div>
     </div>
   );
 }
