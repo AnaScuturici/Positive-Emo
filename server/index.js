72b93839e3f13f0cccb73ad707a7815aa6711370
ana scuturici <anascuturici@gmail.com>
2022-01-06 12:25:24 +0900

    fix: add path in server/index

--- server/index.js
+++ server/index.js
@@ -17,6 +17,10 @@ app.get("/api", async (req, res) => {
     res.json(result);
 });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
