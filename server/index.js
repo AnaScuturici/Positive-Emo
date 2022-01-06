d269461601458540a90dc8b3fcb1be928433e6a6
ana scuturici <anascuturici@gmail.com>
2022-01-06 11:37:31 +0900

    add: procfile, static for deployment

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const axios = require("axios");
+const path = require("path");
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, "../build")));
 
 app.get("/api", async (req, res) => {
     const text = "https://www.affirmations.dev";
